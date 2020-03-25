555ff9625ec78ae3f105882ef6baf86197061e40
zhenjess <jessicazhen8@gmail.com>
2020-03-24 20:56:32 -0700

    Set up board template with grid

--- src/tictactoe-view.js
+++ src/tictactoe-view.js
@@ -14,5 +14,17 @@ class View {
         }));
     }
 
-    
+    setupBoard() {
+        const $ul = $("<ul>");
+
+        for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
+            for (let colIdx = 0; colIdx < 3; colIdx++) {
+                let $li = $("<li>");
+                $li.data("pos", [rowIdx, colIdx]);
+
+                $ul.append($li);
+            }
+        }
+        this.$el.append($ul);
+    }
 }
